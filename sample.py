0d013e7f065525ce908c792c7beee3e2a9cb6a3c
Akamine19-ulab <k173303@edu.tut.ac.jp>
2019-07-04 23:32:54 +0900

    commit master

---
+++ sample.py
@@ -0,0 +1,22 @@
+from multiprocessing import Pool
+import time
+
+def f(x):
+    return x*x
+
+
+if __name__ == "__main__":
+    N = [10, 1000, 10000]
+    print("単純実装")
+    for n in N:
+        start = time.time()
+        for x in range(n):
+            f(x)
+        print("n:{} time:{}".format(n, time.time()-start))
+
+    print("並列処理")
+    for n in N:
+        start = time.time()
+        with Pool(processes=4) as pool:
+            pool.map(f, range(n))
+        print("n:{} time:{}".format(n, time.time()-start))   
